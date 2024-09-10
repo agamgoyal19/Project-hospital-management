2e26c503535377bd5cff86ea46dc28818eff1761
agamgoyal19 <agamgoyal71419@gmail.com>
2024-09-09 18:19:04 -0700

    initial repository to GIT HUB

---
+++ index-connection.php
@@ -0,0 +1,10 @@
+<?php
+	$dbcon= mysqli_connect("localhost","id21585270_agamgoyal21","Agamgoyal@71419","id21585270_db2020");
+	$msg=mysqli_connect_error();//if no error it gives ""- empty string
+//if($msg=="")
+//	echo "Connected Successfully";
+//else
+//	echo $msg;
+
+
+?>
